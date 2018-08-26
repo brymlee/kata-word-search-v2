a87a37af19ae4573979499626e18b23583f6a1d6
brymlee <brymlee1911@gmail.com>
2018-08-26 11:02:52 -0700

    Initial commit

---
+++ src/test/java/brymlee/WordSearchTest.java
@@ -0,0 +1,14 @@
+package brymlee;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WordSearchTest{
+
+    @Test
+    public void helloEqualsHello(){
+        assertEquals("hello", "hello");
+    } 
+
+}
